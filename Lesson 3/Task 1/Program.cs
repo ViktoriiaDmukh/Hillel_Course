5c68669978e6d5317d3c20c4a6f6f99e0ace2322
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-10 22:20:39 -0600

    add

---
+++ Lesson 3/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво
+
+        int triple_figuer, reciprocal_number;
+        int digit1, digit2, digit3;
+
+        Console.Write("Please enter triple figuer: ");
+
+        triple_figuer = int.Parse(Console.ReadLine());
+
+        digit1 = triple_figuer / 100;
+
+        digit2 = (triple_figuer - (digit1 * 100)) / 10;
+
+        digit3 = triple_figuer % 10;
+
+        Console.Write($"Reciprocal number is: {digit3}{digit2}{digit1}");
+
+        Console.ReadKey();
+    }
+}
