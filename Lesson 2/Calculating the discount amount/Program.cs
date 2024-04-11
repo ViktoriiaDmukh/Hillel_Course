187bdf7b2c55f2e404e0d927bf4fd4da72d17e27
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-10 21:55:50 -0600

    removing

--- Lesson 2/Calculating the discount amount/Program.cs
+++ Lesson 2/Calculating the discount amount/Program.cs
@@ -1,17 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
-            Console.ReadLine();
+ushort price, discount;
 
-        }
-    }
-}
+Console.WriteLine("Please enter the price of the product: ");
 
+string price_str = Console.ReadLine();
+
+price = Convert.ToUInt16(price_str);
+
+Console.WriteLine("Please enter the discount amount: ");
+
+string discount_str = Console.ReadLine();
+
+discount = Convert.ToUInt16(discount_str);
+
+ushort amount_of_discount;
+
+Console.WriteLine("Current price is " + (System.Math.Round((double)(price - (price * discount / 100)))));
+
+Console.WriteLine("Amount of discount is: " + (price * discount / 100));
+
+Console.ReadLine();
 
