59e2c72bcf759ce508a8d9dee1e94b4ead4af213
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-10 23:12:43 -0600

    fifth

---
+++ Lesson 3/fifth task/Fifth task/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Fifth_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Користувач вводить радіус круга. Напишіть програму, яка обчислює площу цього круга і виводить в консоль результат. 
+
+            double round_radius, circle_area;
+
+            Console.Write("Please enter the radius of the round, could be not integer amount: ");
+
+            round_radius = double.Parse(Console.ReadLine());
+
+            circle_area = 2 * Math.PI * round_radius;
+
+            Console.Write($"The area of the circle is: {circle_area}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
