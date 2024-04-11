1b027f714ae5be24333ce080f2f0dcd896731d1c
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-10 22:43:16 -0600

    add

---
+++ Lesson 3/Second task/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Second_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написати програму, яка обчислює квадрат будь-якого введеного числа (бажано використати Math)
+
+            Console.Write("Please enter any number that you'd like to squear: ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            double squear_number = Math.Pow(number,2);
+
+            Console.Write($"Squear number of the input number is: {squear_number}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
