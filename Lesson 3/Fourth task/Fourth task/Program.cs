00a818840966bf93c2f6fef909bfee34ad90fdd4
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-10 23:03:31 -0600

    add

---
+++ Lesson 3/Fourth task/Fourth task/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Fourth_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+            Console.Write("Please enter first value: ");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter second value: ");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.Write($"Arithmetic mean of the two values is: {result}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
