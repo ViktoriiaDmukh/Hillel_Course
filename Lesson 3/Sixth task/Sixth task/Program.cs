95d385d51d58b9b64a73caa51a70082e51e809dd
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-16 21:56:56 -0600

    add home work

---
+++ Lesson 3/Sixth task/Sixth task/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sixth_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної та використання суми/множення. 
+
+            int firstValue, secondValue;
+            double percent;
+
+            firstValue = 10; secondValue = 20;
+
+            percent = (firstValue + secondValue) / 100;
+            
+
+
+
+        }
+    }
+}
---
+++ Lesson 4/Lesson4.1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter first value to compare: ");
+
+            int firstValue = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter second value to compare: ");
+
+            int secondValue = int.Parse(Console.ReadLine());
+
+            if (firstValue == secondValue)
+            {
+                Console.Write("The entered values are equal!");
+            }
+            else if (firstValue > secondValue)
+            {
+                Console.Write($"The value {firstValue} is greater then second one.");
+            }
+            else
+            {
+                Console.Write($"The value {secondValue} is greater then first one.");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
