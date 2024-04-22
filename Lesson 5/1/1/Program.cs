6c4d841705e1b59d23d63455991953f4af1075bb
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-21 23:23:35 -0600

    add task 1 for lesson 5

---
+++ Lesson 5/1/1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений.
+             * Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
+             */
+
+            Console.Write("Please enter password: ");
+          
+            string password = Console.ReadLine();
+            int i = 1;
+            while (i <= 10)
+            {
+                if(password == "root")
+                {
+                    Console.WriteLine("Access allowed!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entered password is wrong!");
+                    break;
+                }
+                i++;
+            }
+
+                         
+            Console.ReadKey();
+        }
+
+    }
+}
