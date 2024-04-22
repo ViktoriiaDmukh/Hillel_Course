79d0dd432b9dc590397a1909e94e1790ea9f1b93
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-22 01:25:42 -0600

    add

---
+++ Lesson 5/3/Lesson_5.3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lesson_5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Таблиця множення
+
+            
+
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine('\n');
+
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write($"{j} * {i} = {i * j}" + "        ");
+                }
+                i++;
+            }
+
+            Console.WriteLine('\t');
+
+            i = 6;
+            while (i <= 10)
+            {
+                Console.WriteLine('\n');
+
+                for (int j = 6; j <= 10; j++)
+                {
+                    Console.Write($"{j} * {i} = {i * j}" + "        ");
+                }
+                i++;
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
