cf5bf427a775ace5d9c1342df53c92f6b26a0f47
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-04-26 21:02:18 -0600

    add lesson 6.1

---
+++ Lesson 6/6.1/Lesson 6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lesson_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Задати масив із 10 елементів. Заповнити цей масив рандомними числами від -100 до 100.
+             * Знайти кількість додатніх чисел у масиві. Вивести на екран масив і кількість порахованих чисел. 
+             */
+
+            int[] myArray = new int[10];
+
+           var random = new Random();
+           
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = random.Next(-101, 101);
+                Console.Write(myArray[i] + "\t");
+            }
+
+            int integer = 0;
+
+            for (int i = 0; i < myArray.Length;i++)
+            {
+                if (myArray[i] > 0)
+                {
+                    integer++;
+                }
+            }
+           
+            Console.WriteLine($"\n\nThe amount of integers: {integer} ");
+
+            Console.ReadKey();
+        }
+    }
+}
