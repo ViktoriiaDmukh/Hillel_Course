b2bafd11902b5b654de79991a2281764c71d3f70
ViktoriiaDmukh <vikadmukh@gmail.com>
2024-05-19 18:33:19 -0600

    add lesson

---
+++ Lesson 11/Lesson 11_1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Створіть клас UkrainianStringUtils, який буде містити корисні методи для обробки рядків українською мовою.
+
+Клас UkrainianStringUtils та методи створити статичними, тобто позначаючи модифікаторами static (с. 141-142)
+
+1) Реалізуйте метод CountVowels, який приймає рядок і повертає кількість голосних літер у ньому.
+
+2) CountConsonants: Метод для підрахунку кількості приголосних літер у рядку.
+
+3) Реалізуйте метод ReverseString, який приймає рядок і повертає його обернену версію.
+
+4) RemoveDuplicates: Метод для видалення дублікатів символів з рядка, залишаючи тільки перше входження кожного символу.
+
+5) RemovePunctuation: Метод для видалення всіх знаків пунктуації з рядка.
+
+Після створення бібліотеки, створіть консольну програму, яка використовує цю бібліотеку для демонстрації роботи методів.
+ */
+
+
+//
+using MyLib;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Lesson_11_1
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Будь ласка введіть речення: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(UkrainianStringUtils.CountVowels(input));
+            Console.WriteLine(UkrainianStringUtils.CountConsonants(input));
+            Console.WriteLine(UkrainianStringUtils.ReverseString(input));
+            Console.WriteLine(UkrainianStringUtils.RemovePunctuation(input));
+            Console.WriteLine(UkrainianStringUtils.RemoveDuplicates(input));
+            
+            
+        }
+    }
+}
---
+++ Lesson 11/MyLib/UkrainianStringUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public static class UkrainianStringUtils
+    {
+
+        public static string CountVowels(string input)
+        {
+
+            if (string.IsNullOrEmpty(input))
+                return "Ви нічого не ввели!";
+            string vowels = "аеєиіїоуюяАЕЄИІЇОУЮЯ";
+            int count = input.Count(c => vowels.Contains(c));
+            return $"Кількість голосних в реченні {count}.";
+        }
+
+        public static string CountConsonants(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "Ви нічого не ввели!";
+            string consonants = "бвгґджзйклмнпрстфхцчшщБВГҐДЖЗЙКЛМНПРСТФХЦЧШЩ";
+            int count = input.Count(c => consonants.Contains(c));
+            return $"Кількість приголосних в реченні: {count}";
+        }
+
+        public static string ReverseString(string input)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            if (input == null || input.Length == 0)
+                return "Ви нічого не ввели";
+            char[] array = input.ToCharArray();
+
+            Array.Reverse(array);
+            return new string(array);
+        }
+        public static string RemoveDuplicates(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "Ви нічого не ввели!";
+            var letters = input.Split(' ');
+            StringBuilder resultBuilder = new StringBuilder();
+            foreach (var letter in letters)
+            {
+                HashSet<char> seen = new HashSet<char>();
+                foreach (char c in letter)
+                {
+                    if (!seen.Contains(c))
+                    {
+                        seen.Add(c);
+                        resultBuilder.Append(c);
+                    }
+                }
+                resultBuilder.Append(' ');
+            }
+            return resultBuilder.ToString();
+        }
+        public static string RemovePunctuation(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "Ви нічого не ввели!";
+            return Regex.Replace(input, @"[\p{P}\p{S}]", "");
+
+        }
+    }
+}
